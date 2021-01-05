2a7508f813b736ba046ddf8517649a16db08490c
Leonardo DaVchezt <davchezt@gmail.com>
2021-01-05 21:58:02 +0700

    Updating headers

--- index.js
+++ index.js
@@ -19,13 +19,14 @@ const ip = "0.0.0.0";
 const port = 8080;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.setHeader('Access-Control-Allow-Credentials', true);
   if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    res.setHeader("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
   }
   next();
